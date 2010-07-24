935ca7349d04eb91b8dbf25a263c43e6c92e4d6b
Paul Picazo <ppicazo@gmail.com>
2010-07-24 13:20:34 -0700

    remove override

--- src/org/smerty/zooborns/ZooBorns.java
+++ src/org/smerty/zooborns/ZooBorns.java
@@ -69,7 +69,6 @@ public class ZooBorns extends Activity {
 
 			gridview.setOnItemClickListener(new OnItemClickListener() {
 
-				@Override
 				public void onItemClick(AdapterView<?> arg0, View arg1,
 						int arg2, long arg3) {
 					// TODO Auto-generated method stub
